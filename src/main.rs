3b87905150f20b6e50ee7360dafe7de0e492595a
Nick Garbalau <garbalaunick@gmail.com>
2023-05-03 09:01:23 +0300

    Added information about data types and variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
+
+    // * Data Types
+    // Boolean - true, false
+    // Integer - 1, 2, 50, 99, -2
+    // Double / Float - 1.1, 5.5, 2.0
+    // Character - 'A','$'
+    // String - "Hello", "This is string"
+
+    // * Variables
+    // Assign data to temporary memory location
+    // Can be set to any value & type
+    // Immutable by default, but can be mutable
+
+    // Examples
+    // let two = 2;
+    // let hello = "Hello";
+    // let j = 'J';
+    // let my_half = 0.5;
+    // let mut my_name = "Nick";
+    // let quit_program = false;
+    // let your_half = my_half;
 }
